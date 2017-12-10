a7f17ae710cd8e769bbe11d88052fab6dc252779
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-10 15:00:38 -0500

    creates HomeContainer component

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../components/Header';
+import HomeContainer from '../components/HomeContainer';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
         <Header />
+        <HomeContainer />
       </div>
     );
   }
---
+++ app/components/HomeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class HomeContainer extends React.Component {
+  render () {
+    return (
+      <div className='home-container'>
+        <h1 className='header-text'>Enter a City</h1>
+      </div>
+    );
+  }
+}
