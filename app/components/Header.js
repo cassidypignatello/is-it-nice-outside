b1e027f3b116d3701200b70743352613b7499429
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-10 15:48:36 -0500

    renders CityInput inside navbar

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CityInput from './CityInput';
 
 const Header = (props) => {
   return (
     <div className='navbar'>
       <h1>Is It Nice Outside?</h1>
+      <CityInput flexDirection='row' />
     </div>
   );
 }
