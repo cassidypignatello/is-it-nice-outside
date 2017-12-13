b219ea2672bb1974dc41a79790603008ca23807e
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-13 18:43:48 -0500

    adds route in nav back to homepage

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import CityInput from './CityInput';
 
 const Header = (props) => {
   return (
     <div className='navbar'>
-      <h1>Is It Nice Outside?</h1>
+      <Link to='/'>
+        <h1>Is It Nice Outside?</h1>
+      </Link>
       <CityInput flexDirection='row' />
     </div>
   );
