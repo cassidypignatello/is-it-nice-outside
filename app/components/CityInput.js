6cb497b744a0c9883420cbc9beaa25d861899f7f
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-13 12:52:34 -0500

    moves state into parent container and refactors CityInput into stateless function component

--- app/components/CityInput.js
+++ app/components/CityInput.js
@@ -1,57 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import api from '../utils/api';
 import { Link } from 'react-router-dom';
 
-export default class CityInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleChange(e) {
-    const value = e.target.value;
-    this.setState({
-      city: value
-    });
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    api.fetchCurrentWeather(this.state.city);
-  }
-  handleClick(e) {
-    if (this.state.city === '') {
-      e.preventDefault();
-    }
-  }
-  render() {
-    return (
-      <form
-        style={{display: 'flex', flexDirection: this.props.flexDirection, alignItems: 'center'}}
-        onSubmit={this.handleSubmit}>
-        <input
-          id='city'
-          placeholder='New York'
-          type='text'
-          autoComplete='off'
-          value={this.state.city}
-          onChange={this.handleChange}
-        />
-        <Link
-          to='/forecast'
-          className='weather-btn'
-          onClick={this.handleClick}>
-          Get Weather
-        </Link>
-      </form>
-    );
-  }
-}
+const CityInput = (props) => (
+  <form
+    style={{display: 'flex', flexDirection: props.flexDirection, alignItems: 'center'}}
+    onSubmit={props.handleSubmit}>
+    <input
+      id='city'
+      placeholder='New York'
+      type='text'
+      autoComplete='off'
+      value={props.city}
+      onChange={props.handleChange}
+    />
+    <Link
+      to='/forecast'
+      className='weather-btn'
+      onClick={props.handleClick}>
+      Get Weather
+    </Link>
+  </form>
+);
 
 CityInput.defaultProps = {
   flexDirection: 'column'
@@ -60,3 +31,4 @@ CityInput.defaultProps = {
 CityInput.propTypes = {
   flexDirection: PropTypes.string.isRequired
 }
+export default CityInput;
