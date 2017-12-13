bac70a834cbf294238903cd982b962bc606a6d3e
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-13 18:26:49 -0500

    Revert "moves state into parent container and refactors CityInput into stateless function component"
    This reverts commit 6cb497b744a0c9883420cbc9beaa25d861899f7f.
    
    rolls back previous changes

--- app/components/HomeContainer.js
+++ app/components/HomeContainer.js
@@ -1,43 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CityInput from './CityInput';
-import api from '../utils/api';
 
 export default class HomeContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleChange(e) {
-    const value = e.target.value;
-    this.setState({
-      city: value
-    });
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    api.fetchCurrentWeather(this.state.city);
-  }
-  handleClick(e) {
-    if (this.state.city === '') {
-      e.preventDefault();
-    }
-  }
   render () {
     return (
       <div className='home-container'>
         <h1 className='header-text'>Enter a City</h1>
-        <CityInput
-          city={this.state.city}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-          handleClick={this.handleClick}
-        />
+        <CityInput />
       </div>
     );
   }
