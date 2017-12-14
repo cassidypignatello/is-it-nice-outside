160911a369d70ee7e2d2522374c211f7aa99b2c0
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-13 23:38:04 -0500

    renders Loading component while request to api is being made

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,13 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import api from '../utils/api';
 import queryString from 'query-string';
+import Loading from './Loading';
 
 export default class Forecast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
   componentDidMount() {
     const query = queryString.parse(this.props.location.search);
-    api.fetchCurrentWeather(query.city);
+    api.fetchCurrentWeather(query.city)
+      .then((response) => {
+        console.log(response.data.name);
+        console.log(response.data.weather[0].description);
+        console.log('The temperature is currently ' + response.data.main.temp);
+        console.log('Humidity: ' + response.data.main.humidity);
+        console.log('Low ' + response.data.main.temp_min);
+        console.log('High ' + response.data.main.temp_max);
+        this.setState({
+          loading: false
+        })
+      });
   }
   render() {
+    if (this.state.loading) {
+      return (
+        <Loading />
+      );
+    }
+
     return (
       <p>Forecast!</p>
     );
---
+++ app/components/Loading.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+}
+
+export default class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: this.props.text
+    };
+  }
+  componentDidMount() {
+    const { text, speed } = this.props;
+    const stopper = text + '...';
+    this.setInterval = window.setInterval(() => {
+      this.state.text === stopper ?
+      this.setState(() => ({ text: text })) :
+      this.setState((prevState) => ({ text: prevState.text + '.' }));
+    }, speed);
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
+    );
+  }
+}
+
+Loading.PropTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired
+}
+
+Loading.defaultProps = {
+  text: 'Loading',
+  speed: 200
+}
