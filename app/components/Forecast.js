7b0200c20b6036ae091aaa0f764a45b1aeff56f7
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-14 20:21:22 -0500

    fetches current weather and forecast via ajax request, adds error handling

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import api from '../utils/api';
 import queryString from 'query-string';
 import Loading from './Loading';
@@ -9,16 +10,24 @@ export default class Forecast extends React.Component {
     super(props);
     this.state = {
       loading: true,
-      data: null
+      data: null,
+      error: null
     };
   }
   componentDidMount() {
     const query = queryString.parse(this.props.location.search);
-    api.fetchCurrentWeather(query.city)
+    api.getWeatherData(query.city)
       .then((data) => {
+        if (data === null) {
+          return this.setState({
+            loading: false,
+            error: "Sorry, I can't seem to find that city. Can you check the spelling and try again?"
+          });
+        }
         this.setState({
           loading: false,
-          data: data
+          data: data,
+          error: null
         });
       });
   }
@@ -28,15 +37,22 @@ export default class Forecast extends React.Component {
         <Loading />
       );
     }
-
+    if (this.state.error) {
+      return (
+        <div>
+          <p>{this.state.error}</p>
+          <Link style={{color: 'black'}} to='/'>Reset</Link>
+        </div>
+      );
+    }
     return (
       <div className='container'>
         <div className='current-conditions'>
-          <h1>The current weather for {this.state.data.name} is {this.state.data.weather[0].description}.</h1>
-          <p>Current Temperature: {Math.round(this.state.data.main.temp)}&deg;</p>
-          <p>Humidity: {this.state.data.main.humidity}%</p>
-          <p>Today's Low: {Math.round(this.state.data.main.temp_min)}&deg;</p>
-          <p>Today's High: {Math.round(this.state.data.main.temp_max)}&deg;</p>
+          <h1>The current weather for {this.state.data.currentConditions.name} is {this.state.data.currentConditions.weather[0].description}.</h1>
+          <p>Current Temperature: {Math.round(this.state.data.currentConditions.main.temp)}&deg;</p>
+          <p>Humidity: {this.state.data.currentConditions.main.humidity}%</p>
+          <p>Today's Low: {Math.round(this.state.data.currentConditions.main.temp_min)}&deg;</p>
+          <p>Today's High: {Math.round(this.state.data.currentConditions.main.temp_max)}&deg;</p>
         </div>
       </div>
     );
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
-
 const id = 'fdeb56ea588891439fd8f658fe75e6a7';
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
 
 module.exports = {
   fetchCurrentWeather: function(city) {
@@ -9,9 +13,19 @@ module.exports = {
   },
   fetchForecast: function(city) {
     const encodedURI = window.encodeURI(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=imperial&type=accurate&APPID=${id}&cnt=5`)
-    return axios.get(encodedURI)
-      .then((response) => {
-        console.log(response);
-      })
+    return axios.get(encodedURI).then((response) => response.data);
+  },
+  getWeatherData: function(city) {
+    return axios.all([
+      this.fetchCurrentWeather(city),
+      this.fetchForecast(city)
+    ]).then((response) => {
+      const currentConditions = response[0];
+      const forecast = response[1];
+      return {
+        currentConditions: currentConditions,
+        forecast: forecast
+      }
+    }).catch(handleError);
   }
 }
