e4f5225f9fb83064fe4aeaf75e5663ee125ba717
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-15 16:17:21 -0500

    renders ForecastGrid

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,8 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import api from '../utils/api';
+import helpers from '../utils/helpers';
 import queryString from 'query-string';
 import Loading from './Loading';
+
+const ForecastGrid = (props) => (
+  <div className='forecast-container'>
+    <h1>5-Day Forecast</h1>
+    <ul className='forecast-list'>
+      {props.forecast.list.map((day) => {
+        const date = helpers.getDate(day.dt);
+        return (
+          <li key={day.dt} className='forecast-item'>
+            <ul className='space-list-items'>
+              <li>
+                <img className='weather-icon' src={`/app/images/weather-icons/${day.weather[0].icon}.svg`}/>
+              </li>
+              <li className='forecast-date'>{date}</li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+);
 
 export default class Forecast extends React.Component {
   constructor(props) {
@@ -18,6 +40,7 @@ export default class Forecast extends React.Component {
     const query = queryString.parse(this.props.location.search);
     api.getWeatherData(query.city)
       .then((data) => {
+        console.log(data);
         if (data === null) {
           return this.setState({
             loading: false,
@@ -49,12 +72,14 @@ export default class Forecast extends React.Component {
       <div className='container'>
         <div className='current-conditions'>
           <h1>The current weather for {this.state.data.currentConditions.name} is {this.state.data.currentConditions.weather[0].description}.</h1>
-          <img className='weather-icon' src={`/app/images/weather-icons/${this.state.data.currentConditions.weather[0].icon}.svg`}></img>
+          <img className='weather-icon' src={`/app/images/weather-icons/${this.state.data.currentConditions.weather[0].icon}.svg`}/>
           <p>Current Temperature: {Math.round(this.state.data.currentConditions.main.temp)}&deg;</p>
           <p>Humidity: {this.state.data.currentConditions.main.humidity}%</p>
           <p>Today's Low: {Math.round(this.state.data.currentConditions.main.temp_min)}&deg;</p>
           <p>Today's High: {Math.round(this.state.data.currentConditions.main.temp_max)}&deg;</p>
         </div>
+        <hr />
+        <ForecastGrid forecast={this.state.data.forecast} />
       </div>
     );
   }
