a24c34c5b2e9d31d543c20142cee0e3126486a40
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-18 17:42:54 -0500

    refactors Home

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HomeContainer from './HomeContainer';
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div className='container'>
-        <HomeContainer />
-      </div>
-    );
-  }
-}
+const Home = (props) => (
+  <div className='container'>
+    <HomeContainer />
+  </div>
+);
+
+export default Home;
