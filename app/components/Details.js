b80788fa1250d6bc1307fb0a15e888ad2ce7ca43
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-17 22:34:32 -0500

    refactors Details

--- app/components/Details.js
+++ app/components/Details.js
@@ -4,16 +4,16 @@ import ForecastItem from './ForecastItem';
 export default class Details extends React.Component {
   render() {
     const props = this.props.location.state;
-    console.log(props);
+    const { city, weather, humidity, temp } = props;
     return (
       <div>
         <ForecastItem day={props} />
         <div className='details-container'>
-          <p>{props.city}</p>
-          <p>{props.weather[0].description}</p>
-          <p>Humidity: {props.humidity}%</p>
-          <p>Low Temperature: {Math.round(props.temp.min)}&deg;</p>
-          <p>High Temperature: {Math.round(props.temp.max)}&deg;</p>
+          <p>{city}</p>
+          <p>{weather[0].description}</p>
+          <p>Humidity: {humidity}%</p>
+          <p>Low Temperature: {Math.round(temp.min)}&deg;</p>
+          <p>High Temperature: {Math.round(temp.max)}&deg;</p>
         </div>
       </div>
     );
