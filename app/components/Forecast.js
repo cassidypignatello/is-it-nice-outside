c45f965718ac06015642ed3394e511e2c720e5d8
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-18 16:50:04 -0500

    refactors Forecast

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -8,22 +8,16 @@ import ForecastItem from './ForecastItem';
 import WeatherIcon from './WeatherIcon';
 
 export default class Forecast extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      data: null,
-      error: null
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.makeRequest = this.makeRequest.bind(this);
-  }
-  componentDidMount() {
+  state = {
+    loading: true,
+    data: null,
+    error: null
+  };
+  componentDidMount = () => {
     this.city = queryString.parse(this.props.location.search).city;
     this.makeRequest(this.city);
   }
-  componentWillReceiveProps(nextProps) {
-    console.log(nextProps);
+  componentWillReceiveProps = (nextProps) => {
     this.city = queryString.parse(nextProps.location.search).city;
     this.makeRequest(this.city);
   }
@@ -33,7 +27,6 @@ export default class Forecast extends React.Component {
     });
     getWeatherData(city)
       .then((data) => {
-        console.log(data);
         if (data === null) {
           return this.setState({
             loading: false,
@@ -55,40 +48,30 @@ export default class Forecast extends React.Component {
     })
   }
   render() {
-    if (this.state.loading) {
-      return (
-        <Loading />
-      );
-    }
-    if (this.state.error) {
-      return (
-        <div>
-          <p>{this.state.error}</p>
-          <Link style={{color: 'black'}} to='/'>Reset</Link>
-        </div>
-      );
-    }
-
-    return (
+    const { loading, error, data } = this.state;
+    return loading ?
+      <Loading /> :
+      error ?
+      <div>
+        <p>{error}</p>
+        <Link style={{color: 'black'}} to='/'>Reset</Link>
+      </div> :
       <div className='container'>
         <div className='current-conditions'>
-          <h1>The current weather for {this.state.data.currentConditions.name} is {this.state.data.currentConditions.weather[0].description}.</h1>
-          <WeatherIcon iconId={this.state.data.currentConditions.weather[0].icon} />
-          <p>Current Temperature: {Math.round(this.state.data.currentConditions.main.temp)}&deg;</p>
-          <p>Humidity: {this.state.data.currentConditions.main.humidity}%</p>
-          <p>Today's Low Temperature: {Math.round(this.state.data.currentConditions.main.temp_min)}&deg;</p>
-          <p>Today's High Temperature: {Math.round(this.state.data.currentConditions.main.temp_max)}&deg;</p>
+          <h1>The current weather for {data.currentConditions.name} is {data.currentConditions.weather[0].description}.</h1>
+          <WeatherIcon iconId={data.currentConditions.weather[0].icon} />
+          <p>Current Temperature: {Math.round(data.currentConditions.main.temp)}&deg;</p>
+          <p>Humidity: {data.currentConditions.main.humidity}%</p>
+          <p>Today's Low Temperature: {Math.round(data.currentConditions.main.temp_min)}&deg;</p>
+          <p>Today's High Temperature: {Math.round(data.currentConditions.main.temp_max)}&deg;</p>
         </div>
         <hr />
         <div className='forecast-container'>
           <h1>5-Day Forecast</h1>
           <ul className='forecast-list'>
-            {this.state.data.forecast.list.map((day) => {
-              return <ForecastItem onClick={this.handleClick.bind(this, day)} key={day.dt} day={day} />
-            }, this)}
+            {data.forecast.list.map((day) => <ForecastItem onClick={() => this.handleClick(day)} key={day.dt} day={day} />)}
           </ul>
         </div>
       </div>
-    );
   }
 }
