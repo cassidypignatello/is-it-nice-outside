3946f6a755df569d6b037db8268395132428b69c
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-18 18:04:05 -0500

    changes webpack config require statements to import

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
 
 var config = {
   entry: ['babel-polyfill', './app/index.js'],
