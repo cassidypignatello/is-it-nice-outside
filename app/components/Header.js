3aa6d2a346144588a674c8616863f34dabf1394d
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-18 17:41:22 -0500

    refactors Header

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import CityInput from './CityInput';
 
-const Header = (props) => {
-  return (
-    <div className='navbar'>
-      <Link to='/'>
-        <h1>Is It Nice Outside?</h1>
-      </Link>
-      <CityInput flexDirection='row' />
-    </div>
-  );
-}
+const Header = (props) => (
+  <div className='navbar'>
+    <Link to='/'>
+      <h1>Is It Nice Outside?</h1>
+    </Link>
+    <CityInput flexDirection='row' />
+  </div>
+);
 
 export default Header;
