0e071910d1d1c3bf34e076a185f5b7c341561ee0
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-18 17:44:41 -0500

    refactors HomeContainer to stateless functional component

--- app/components/HomeContainer.js
+++ app/components/HomeContainer.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CityInput from './CityInput';
 
-export default class HomeContainer extends React.Component {
-  render () {
-    return (
-      <div className='home-container'>
-        <h1 className='header-text'>Enter a City</h1>
-        <CityInput />
-      </div>
-    );
-  }
-}
+const HomeContainer = (props) => (
+  <div className='home-container'>
+    <h1 className='header-text'>Enter a City</h1>
+    <CityInput />
+  </div>
+);
+
+export default HomeContainer;
